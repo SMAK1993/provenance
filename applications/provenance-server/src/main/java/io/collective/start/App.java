87671ecb919cd46a456c0b883beea0a40321924c
Adnan Karim <adnan.karim.93@gmail.com>
2024-02-02 05:07:59 -0500

    Completed App Tests

--- applications/provenance-server/src/main/java/io/collective/start/App.java
+++ applications/provenance-server/src/main/java/io/collective/start/App.java
@@ -4,11 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.collective.articles.ArticleDataGateway;
 import io.collective.articles.ArticleRecord;
 import io.collective.articles.ArticlesController;
+import io.collective.endpoints.EndpointDataGateway;
+import io.collective.endpoints.EndpointTask;
+import io.collective.endpoints.EndpointWorkFinder;
+import io.collective.endpoints.EndpointWorker;
 import io.collective.restsupport.BasicApp;
 import io.collective.restsupport.NoopController;
+import io.collective.restsupport.RestTemplate;
+import io.collective.workflow.WorkScheduler;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -23,8 +30,11 @@ public class App extends BasicApp {
     public void start() {
         super.start();
 
-        { // todo - start the endpoint worker
-
+        {
+            EndpointWorkFinder finder = new EndpointWorkFinder(new EndpointDataGateway());
+            EndpointWorker workers = new EndpointWorker(new RestTemplate(), articleDataGateway);
+            WorkScheduler<EndpointTask> scheduler = new WorkScheduler<>(finder, Collections.singletonList(workers), 300);
+            scheduler.start();
         }
     }
 
